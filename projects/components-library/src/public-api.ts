6e40efad309b193aaf8139107d4111826c15b459
amillanbc <amillan@bluecorela.com>
2025-03-07 13:13:52 -0500

    select component and utils update

--- projects/components-library/src/public-api.ts
+++ projects/components-library/src/public-api.ts
@@ -9,12 +9,7 @@ export * from './lib/components/gb-toggle/gb-toggle.component';
 export * from './lib/components/gb-checkbox/gb-checkbox.component';
 export * from './lib/components/gb-generic-modal/gb-generic-modal.component';
 export * from './lib/components/gb-select/gb-select.component';
-export * from './lib/components/gb-select-content/gb-select-content.component';
 export * from './lib/components/gb-progress-bar/gb-progress-bar.component';
 
 //SERVICES
 export * from './lib/services/utils.service';
-
-//TYPES
-export * from './lib/types/FormObject';
-export * from './lib/types/User';
