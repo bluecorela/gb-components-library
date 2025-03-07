6fed389aac71401a0ffdfee6d7950eb57b631e76
amillanbc <amillan@bluecorela.com>
2025-03-07 15:25:21 -0500

    gb icon export in public-api.ts

--- projects/components-library/src/public-api.ts
+++ projects/components-library/src/public-api.ts
@@ -10,6 +10,7 @@ export * from './lib/components/gb-checkbox/gb-checkbox.component';
 export * from './lib/components/gb-generic-modal/gb-generic-modal.component';
 export * from './lib/components/gb-select/gb-select.component';
 export * from './lib/components/gb-progress-bar/gb-progress-bar.component';
+export * from './lib/components/gb-icon/gb-icon.component';
 
 //SERVICES
 export * from './lib/services/utils.service';
