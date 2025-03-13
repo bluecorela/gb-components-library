a128ff2ff99eb11058be835c6ed8315c67dffe9d
Indira Cornejo <icornejo@bluecorela.com>
2025-03-13 14:18:14 -0500

    Fix: Add gb-otp component to public API exports

--- projects/components-library/src/public-api.ts
+++ projects/components-library/src/public-api.ts
@@ -11,6 +11,7 @@ export * from './lib/components/gb-generic-modal/gb-generic-modal.component';
 export * from './lib/components/gb-select/gb-select.component';
 export * from './lib/components/gb-progress-bar/gb-progress-bar.component';
 export * from './lib/components/gb-icon/gb-icon.component';
+export * from './lib/components/gb-otp/gb-otp.component';
 
 //SERVICES
 export * from './lib/services/utils.service';
