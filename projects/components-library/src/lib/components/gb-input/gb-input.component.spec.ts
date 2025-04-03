6b4d519cea6b71588d2f6705dcb5876205752506
Jorge Herrera <jherrera@bluecorela.com>
2025-04-03 13:28:14 -0500

    Update: Se actualizan archivos de unitarias

--- projects/components-library/src/lib/components/gb-input/gb-input.component.spec.ts
+++ projects/components-library/src/lib/components/gb-input/gb-input.component.spec.ts
@@ -1,112 +1,85 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { Component } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
 import { FormsModule } from "@angular/forms";
-import { GbInputComponent } from "./gb-input.component";
 import { IonIcon } from "@ionic/angular/standalone";
+import { GbInputComponent } from "./gb-input.component";
+import { IonicModule } from "@ionic/angular";
 
-@Component({
-  selector: "gb-host",
-  standalone: true,
-  template: `<gb-input
-    [type]="type"
-    [value]="value"
-    [color]="color"
-    [level]="level"
-    [icon]="icon"
-    [disabled]="disabled"
-    [passwordToggle]="passwordToggle"
-    [regex]="regex"
-    [required]="required">
-  </gb-input>`,
-  imports: [GbInputComponent],
-})
-export class GbHostComponent {
-  type: "text" | "password" | "email" | "number" = "text";
-  value: string | number = "";
-  color: string = "blue";
-  level: number = 500;
-  icon?: string = "battery-charging-outline";
-  disabled: boolean = false;
-  passwordToggle: boolean = false;
-  regex: string = `^[A-Za-z0-9]{6,}$`;
-  required: boolean = true;
-}
+fdescribe("GbInputComponent", () => {
+  let component: GbInputComponent;
+  let fixture: ComponentFixture<GbInputComponent>;
 
-describe("GbInputComponent", () => {
-  let hostComponent: GbHostComponent;
-  let fixture: ComponentFixture<GbHostComponent>;
-  let inputElement: HTMLInputElement;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [GbHostComponent, FormsModule, IonIcon],
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [IonicModule.forRoot(), GbInputComponent, FormsModule, IonIcon],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(GbHostComponent);
-    hostComponent = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(GbInputComponent);
+    component = fixture.componentInstance;
 
-    inputElement = fixture.nativeElement.querySelector("input");
+    // SETS INPUTS
+    fixture.componentRef.setInput("type", "text");
+    fixture.componentRef.setInput("label", "label");
+    fixture.componentRef.setInput("errHint", "errHint");
+    fixture.componentRef.setInput("okHint", "okHint");
+    fixture.componentRef.setInput("placeholder", "placeholder");
+    fixture.componentRef.setInput("value", "value");
+    fixture.componentRef.setInput("color", "blue");
+    fixture.componentRef.setInput("level", 500);
+    fixture.componentRef.setInput("icon", "icon");
+    fixture.componentRef.setInput("disabled", false);
+    fixture.componentRef.setInput("extraClasses", "extraClasses");
+    fixture.componentRef.setInput("passwordToggle", false);
+    fixture.componentRef.setInput("regex", "regex");
+    fixture.componentRef.setInput("required", false);
+    fixture.componentRef.setInput("min", 0);
+    fixture.componentRef.setInput("max", 0);
+    fixture.componentRef.setInput("identity", "identity");
+    fixture.componentRef.setInput("regexMessages", []);
+    fixture.detectChanges();
+  }));
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
   });
 
-  it("should initialize model with value input", (done) => {
-    hostComponent.value = "Test Value";
-
+  it("should toggle password visibility and cover both if and else cases", () => {
+    fixture.componentRef.setInput("type", "password");
+    fixture.componentRef.setInput("passwordToggle", true);
     fixture.detectChanges();
-
-    setTimeout(() => {
-      fixture.detectChanges();
-
-      inputElement = fixture.nativeElement.querySelector("input");
-
-      expect(inputElement.value).toBe("");
-
-      done();
-    }, 0);
+    component.togglePass();
+    expect(component.isShowingPassword.call(this)).toBeTrue();
+    component.togglePass();
+    expect(component.isShowingPassword.call(this)).toBeFalse();
   });
 
-  it("should apply success class when input value matches regex", (done) => {
-    hostComponent.regex = `^[A-Za-z0-9]{6,}$`;
-    fixture.detectChanges();
-
-    inputElement.value = "Valid1";
-    inputElement.dispatchEvent(new Event("input"));
-    fixture.detectChanges();
-
-    setTimeout(() => {
-      fixture.detectChanges();
-      inputElement = fixture.nativeElement.querySelector("input");
-
-      expect(inputElement.className).toContain("border-gb-success-500");
-      expect(inputElement.className).toContain("focus:border-gb-success-500");
-
-      done();
-    });
+  it("should update focused signal", () => {
+    component.wasFocused();
+    expect(component.focused.call(this)).toBeTrue();
   });
 
-  it("should apply error class when input value does not match regex", (done) => {
-    hostComponent.regex = `^[A-Za-z0-9]{6,}$`;
-    fixture.detectChanges();
-
-    inputElement.value = "abc";
-    inputElement.dispatchEvent(new Event("input"));
-    fixture.detectChanges();
-
-    setTimeout(() => {
-      expect(inputElement.className).toContain("border-gb-error-500");
-      expect(inputElement.className).toContain("focus:border-gb-error-500");
-      done();
-    });
+  it("should test isValid", () => {
+    fixture.componentRef.setInput("regex", undefined);
+    component.isValid();
+    expect(component.isValid()).toBeTrue();
+    fixture.componentRef.setInput("regex", "^\\d{4}$");
+    fixture.componentRef.setInput("min", 25);
+    fixture.componentRef.setInput("value", "20");
+    fixture.componentRef.setInput("valueLoaded", false);
+    component.isValid();
+    expect(component.isValid()).toBeFalse();
+    fixture.componentRef.setInput("max", 25);
+    fixture.componentRef.setInput("value", "30");
+    fixture.componentRef.setInput("valueLoaded", false);
+    component.isValid();
+    expect(component.isValid()).toBeFalse();
   });
 
-  it("should apply error class when required field is empty and loses focus", () => {
-    hostComponent.value = "";
-    fixture.detectChanges();
-
-    inputElement.dispatchEvent(new Event("focusout"));
-    fixture.detectChanges();
-
-    expect(inputElement.className).toContain("border-gb-error-500");
-    expect(inputElement.className).toContain("focus:border-gb-error-500");
+  it("should validateRegex", () => {
+    const reg = ["/[a-z]+/g"];
+    fixture.componentRef.setInput("valueLoaded", true);
+    fixture.componentRef.setInput("regex", reg);
+    fixture.componentRef.setInput("value", "a");
+    fixture.componentRef.setInput("model", "a");
+    expect(component.validateRegex(reg)).toBeTrue();
   });
 });
