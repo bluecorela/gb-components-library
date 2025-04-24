ec2f28217bdd98dfbb06287ff62474653d87625a
Indira Cornejo <icornejo@bluecorela.com>
2025-04-24 09:55:23 -0500

    Fix button class concatenation for consistent styling

--- projects/components-library/src/lib/components/gb-btn/gb-btn.component.ts
+++ projects/components-library/src/lib/components/gb-btn/gb-btn.component.ts
@@ -50,7 +50,7 @@ export class GbBtnComponent {
     let classes =
       "rounded-md inline-flex items-center justify-center py-3 px-7 text-center text-base font-medium gap-2.5 disabled:text-gb-gray-dark-600";
     if (this.fill() == "solid")
-      classes += ` disabled:bg-gb-gray-light-500 disabled:text-gb-dark-600 bg-gb-${color}-500 text-white active:bg-gb-${color}-600 hover:bg-gb-${color}-400 border-gb-${color}-500 active:border-gb-${color}-600 hover:border-gb-${color}-400`;
+      classes += ` disabled:bg-gb-gray-light-500 disabled:text-gb-dark-600 bg-gb-${color}-500 text-white active:bg-gb-${color}-600 border-gb-${color}-500 active:border-gb-${color}-600 hover:border-gb-${color}-400`;
     if (this.fill() == "outline")
       classes += ` active:text-gb-gray-light-400 text-gb-${color}-500 active:text-white bg-gb-no-color border-gb-${color}-500 hover:bg-gb-${color}-50 active:bg-gb-${color}-500  disabled:bg-no-color disabled:border-gb-gray-light-600`;
     classes += ` ${this.extraClasses()}`;
