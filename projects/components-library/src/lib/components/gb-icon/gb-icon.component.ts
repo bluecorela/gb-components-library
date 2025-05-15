b6c8312bfd7758398c5402b27ada5d9e89f4f0cd
amillanbc <amillan@bluecorela.com>
2025-05-15 14:50:22 -0500

    update component to use icons from font

--- projects/components-library/src/lib/components/gb-icon/gb-icon.component.ts
+++ projects/components-library/src/lib/components/gb-icon/gb-icon.component.ts
@@ -1,13 +1,12 @@
 // ##### IONIC & ANGULAR
-import { Component, input, signal, OnInit, computed } from '@angular/core';
-import { IonIcon } from '@ionic/angular/standalone';
-import * as icons from 'ionicons/icons';
-import { addIcons } from 'ionicons';
-
+import { Component, input, signal, OnInit, computed } from "@angular/core";
+import { IonIcon } from "@ionic/angular/standalone";
+import * as icons from "ionicons/icons";
+import { addIcons } from "ionicons";
 @Component({
-  selector: 'gb-icon',
-  templateUrl: './gb-icon.component.html',
-  styleUrls: ['./gb-icon.component.scss'],
+  selector: "gb-icon",
+  templateUrl: "./gb-icon.component.html",
+  styleUrls: ["./gb-icon.component.scss"],
   imports: [IonIcon],
 })
 export class GbIconComponent implements OnInit {
@@ -15,29 +14,27 @@ export class GbIconComponent implements OnInit {
   icon = input.required<string>();
   color = input<string>();
   size = input<string>();
-  fromSrc = input(false);
-  fromFile = input(false);
+  from = input<"font" | "file" | "src" | "ionic">("font");
 
   // ##### SIGNALS
   iconLoaded = signal(false);
 
   // ##### COMPUTED
   iconPath = computed(() => `assets/icon/${this.icon()}.svg`);
+  fontIconClass = computed(() => `icon icon-${this.icon()} text-${this.color()}`);
 
   // ##### LC HOOKS
   async ngOnInit(): Promise<void> {
-    if (this.fromSrc()) return;
-    let icns = { ...icons };
-    if (this.fromFile()) {
+    if (this.from() == "ionic") addIcons(icons);
+    if (this.from() == "file") {
       const response = await fetch(this.iconPath());
       const svgContent = await response.text();
       const key = this.icon();
       const iconsObj: { [key: string]: any } = {};
       iconsObj[key] =
         `data:image/svg+xml;utf8,${svgContent.replace(/fill="#[0-9a-fA-F]{3,6}"/g, 'fill="currentColor"')}`;
-      icns = { ...icns, ...iconsObj };
+      addIcons(iconsObj);
+      this.iconLoaded.update(() => true);
     }
-    addIcons(icns);
-    this.iconLoaded.update(() => true);
   }
 }
