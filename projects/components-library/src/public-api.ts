36bc42075007c95a5b3390e069b5e44866fe71a9
YesuryYB <ybarria@bluecorela.com>
2025-05-16 10:06:51 -0500

    Add gb-skeleton and gb-error-content components

--- projects/components-library/src/public-api.ts
+++ projects/components-library/src/public-api.ts
@@ -13,7 +13,8 @@ export * from "./lib/components/gb-progress-bar/gb-progress-bar.component";
 export * from "./lib/components/gb-icon/gb-icon.component";
 export * from "./lib/components/gb-otp/gb-otp.component";
 export * from "./lib/components/gb-stepper/gb-stepper.component";
-
+export * from "./lib/components/gb-skeleton/gb-skeleton.component";
+export * from "./lib/components/gb-error/gb-error.component";
 //SERVICES
 export * from "./lib/services/utils.service";
 
