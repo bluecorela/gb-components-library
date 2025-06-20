965bb91b9f24e5a638f9dc095fdd2c1a4271aa94
Indira Cornejo <icornejo@bluecorela.com>
2025-06-20 15:51:52 -0500

    Fix: Using class in badge component instead ngClass

--- projects/components-library/src/lib/components/gb-badge/gb-badge.component.ts
+++ projects/components-library/src/lib/components/gb-badge/gb-badge.component.ts
@@ -1,18 +1,23 @@
 import { Component, computed, input } from '@angular/core';
 
 @Component({
-  selector: 'gb-badge',
+  selector: "gb-badge",
   imports: [],
-  templateUrl: './gb-badge.component.html',
-  styleUrl: './gb-badge.component.scss'
+  templateUrl: "./gb-badge.component.html",
+  styleUrl: "./gb-badge.component.scss",
 })
 export class GbBadgeComponent {
-  badgeText = input<string>('Placeholder');
-  badgeType =  input<'solid' | 'outline'>('solid');
+  badgeText = input<string>("Placeholder");
+  badgeType = input<"solid" | "outline">("solid");
 
-  bgColor = input<string>('bg-gb-cyan-200');
-  textColor = input<string>('text-gb-cyan-600');
-  borderColor = input<string>('border-gb-cyan-600');
+  bgColor = input<string>("bg-gb-cyan-200");
+  textColor = input<string>("text-gb-cyan-600");
+  borderColor = input<string>("border-gb-cyan-600");
 
-  borderClass = computed(() => this.badgeType() === 'outline' ? `border ${this.borderColor()}` :'border-0')
+  borderClass = computed(() => (this.badgeType() === "outline" ? `border ${this.borderColor()}` : "border-0"));
+  class = computed(
+    () =>
+      `gb-pl-lg gb-pr-lg gb-pt-xs gb-pb-xs font-red-hat-600 inline-flex items-center rounded-[30px] text-sm ${this.bgColor()} ${this.textColor()} ${this.borderClass()}`,
+  );
 }
+ 
