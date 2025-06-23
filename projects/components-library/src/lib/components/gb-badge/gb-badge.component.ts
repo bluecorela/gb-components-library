f90fadc84436ae01656306885a9cd3fa212ee7eb
Eloy Díaz <eloydiaz@bluecorela.com>
2025-06-23 10:52:00 -0500

    Update badge component - New Icon

--- projects/components-library/src/lib/components/gb-badge/gb-badge.component.ts
+++ projects/components-library/src/lib/components/gb-badge/gb-badge.component.ts
@@ -12,12 +12,14 @@ export class GbBadgeComponent {
 
   bgColor = input<string>("bg-gb-cyan-200");
   textColor = input<string>("text-gb-cyan-600");
+  textSizeWeight = input<string>("text-sm font-red-hat-600");
   borderColor = input<string>("border-gb-cyan-600");
+  borderRadius = input<string>("rounded-[30px]");
+  badgeSpacing = input<string>("gb-pl-lg gb-pr-lg gb-pt-xs gb-pb-xs");
 
   borderClass = computed(() => (this.badgeType() === "outline" ? `border ${this.borderColor()}` : "border-0"));
   class = computed(
     () =>
-      `gb-pl-lg gb-pr-lg gb-pt-xs gb-pb-xs font-red-hat-600 inline-flex items-center rounded-[30px] text-sm ${this.bgColor()} ${this.textColor()} ${this.borderClass()}`,
+      `inline-flex items-center ${this.badgeSpacing()} ${this.borderRadius()} ${this.textSizeWeight()} ${this.bgColor()} ${this.textColor()} ${this.borderClass()}`,
   );
 }
- 
