1ef66afc1e9927e59a0419d9da2c58b61f122f78
amillanbc <amillan@bluecorela.com>
2025-07-10 17:37:13 -0500

    Fix: Update GbSegmentComponent into two way binded

--- projects/components-library/src/lib/components/gb-segment/gb-segment.component.ts
+++ projects/components-library/src/lib/components/gb-segment/gb-segment.component.ts
@@ -11,17 +11,17 @@ import { SegmentTab } from "./gb-segment.interface";
 })
 export class GbSegmentComponent {
   tabs = input.required<SegmentTab[]>();
-  active = input.required<string>();
+  value = input.required<string>();
 
-  tabChange = output<string>();
+  valueChange = output<string>();
 
   updateTab(id: string): void {
-    this.tabChange.emit(id);
+    this.valueChange.emit(id);
   }
 
   tabsClasses = (id: string) => {
     let def = "font-red-hat-500 select-none cursor-pointer text-lg gb-py-2xl gb-px-3xl border-b-2 ";
-    if (id === this.active()) return def + "active-segment-border-bottom border-b-2 bg-gb-blue-25 text-gb-blue-500";
+    if (id === this.value()) return def + "active-segment-border-bottom border-b-2 bg-gb-blue-25 text-gb-blue-500";
     else return def + "segment-border-bottom hover:bg-gb-blue-25 text-gb-text-primary";
   };
 }
