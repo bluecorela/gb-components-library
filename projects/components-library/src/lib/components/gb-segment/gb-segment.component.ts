c1d4818e678283204fd25082df1914de458bbc2f
Jorge Herrera <jherrera@bluecorela.com>
2025-07-17 12:33:37 -0500

    Fix: Se corrigen espaciados de segments

--- projects/components-library/src/lib/components/gb-segment/gb-segment.component.ts
+++ projects/components-library/src/lib/components/gb-segment/gb-segment.component.ts
@@ -20,7 +20,7 @@ export class GbSegmentComponent {
   }
 
   tabsClasses = (id: string) => {
-    let def = "font-red-hat-500 select-none cursor-pointer text-lg gb-py-2xl gb-px-3xl border-b-2 ";
+    let def = "font-red-hat-500 select-none cursor-pointer text-lg gb-py-2xl gb-px-8xl border-b-2 ";
     if (id === this.value()) return def + "active-segment-border-bottom border-b-2 bg-gb-blue-25 text-gb-blue-500";
     else return def + "segment-border-bottom hover:bg-gb-blue-25 text-gb-text-primary";
   };
